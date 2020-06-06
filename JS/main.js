6b0694fc9d28aeb2a34160231a7f134d959d15f1
Nitin Jayant <thenitinjayant@gmail.com>
2020-06-06 11:12:26 +0530

    images changed

--- JS/main.js
+++ JS/main.js
@@ -13,3 +13,12 @@ $(".Navbar-Nav-NavItems-Links").on("click", function (event) {
         );
     }
 });
+
+const sampleImages = {
+    s1:
+        "https://images.unsplash.com/photo-1485988412941-77a35537dae4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1072&q=80.jpg",
+    s2:
+        "https://images.unsplash.com/photo-1434030216411-0b793f4b4173?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80.jpg",
+    s3:
+        "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80.jpg",
+};
